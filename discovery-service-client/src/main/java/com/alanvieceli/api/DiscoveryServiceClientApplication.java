5c6a2bf2a3fa8c148125675c75875ca8ef326a82
Alan Vieceli <alansvieceli@gmail.com>
2018-12-07 14:18:28 -0200

    discovery-service-client

---
+++ discovery-service-client/src/main/java/com/alanvieceli/api/DiscoveryServiceClientApplication.java
@@ -0,0 +1,14 @@
+package com.alanvieceli.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DiscoveryServiceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceClientApplication.class, args);
+	}
+}
---
+++ discovery-service-client/src/main/java/com/alanvieceli/api/controller/ServiceInstanceRestController.java
@@ -0,0 +1,27 @@
+package com.alanvieceli.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+
+@RestController
+public class ServiceInstanceRestController {
+	
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+    
+    //http://localhost:8080/service-instances/discovery-service-client
+
+}
