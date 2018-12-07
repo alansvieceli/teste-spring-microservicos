97e1d2ec4d8fa41912aa11775986aa9381d1297a
Alan Vieceli <alansvieceli@gmail.com>
2018-12-07 14:18:08 -0200

    discovery-service-server

---
+++ discovery-service-server/src/main/java/com/alanvieceli/api/DiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.alanvieceli.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+		//http://localhost:8761/eureka/apps
+	}
+}
